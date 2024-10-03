00adac5012c171a070635a75db59023d54768086
Daniel Kozlík <daniel.kozlik@infis.cz>
2024-10-03 15:24:23 +0200

    funguje

---
+++ src/Podminky.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+import java.nio.file.Files;
+import java.util.Random;
+public class Podminky {
+    public static void main(String[] args) {
+        //Scanner sc = new Scanner(System.in);
+        //System.out.println("Zadejte libovolné číslo.");
+        //int a = sc.nextInt();
+        //if (a % 5 == 0) {
+        //System.out.println("Číslo je dělitelné pěti.");
+        //} else {
+        //System.out.println("Číslo není dělitelné pěti");
+        //}
+
+
+        //Random hod = new Random();
+        //int hodKostkou1 = hod.nextInt(1, 7);
+        //int hodKostkou2 = hod.nextInt(1, 7);
+
+        //System.out.println(hodKostkou1);
+        //System.out.println(hodKostkou2);
+
+        //if (hodKostkou1 + hodKostkou2 >= 10) {
+            //System.out.println("Dobrý hod");
+        //} else if (hodKostkou1 == hodKostkou2) {
+            //System.out.println("Dobrý hod");
+        //} else {
+            //System.out.println("Nic");
+        //}
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Zadejte libovolné číslo.");
+        int a = sc.nextInt();
+        if (a )
+    }
+}
