03c0eece53d06007f64a4aee38a3abcd3af2c376
daniel.kozlik <daniel.kozlik@infis.cz>
2024-10-14 13:15:23 +0200

    co ja vim

---
+++ src/Switch.java
@@ -0,0 +1,21 @@
+public class Switch {
+    public static void main(String[] args) {
+        int day = 7;
+        switch (day) {
+            case 5:
+                System.out.println("je sobota");
+                break;
+            case 2:
+                System.out.println("je neděle");
+                break;
+            default:
+                System.out.println("Je pracovní týden");
+                break;
+        }
+    }
+}
+// 3 priklady
+// AND
+// OR + Random
+// sleva, scenar
+// ctrl + alt + l
---
+++ src/While.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+public class While {
+    public static void main(String[] args) {
+        // vypiste vsechny suda cisla od 0 - 1000
+        int cisla = 0;
+
+        while (cisla <= 1000) {
+            System.out.println(cisla);
+            cisla += 2;
+        }
+
+        //simulujte hod kostkou dokud nepadne 6
+        Random kostka = new Random();
+
+        int hod;
+        do {
+            hod = kostka.nextInt(1, 7);
+            System.out.println("Hodili jste. " + hod);
+        } while (hod != 6);
+    }
+}
