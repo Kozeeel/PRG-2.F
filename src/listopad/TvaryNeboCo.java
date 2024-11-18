849ef4c131040048c5921f9bd27b5a1f35112fdf
daniel.kozlik <daniel.kozlik@infis.cz>
2024-11-18 13:18:27 +0100

    hjzufth

---
+++ src/listopad/TvaryNeboCo.java
@@ -0,0 +1,20 @@
+package listopad;
+
+public class TvaryNeboCo {
+    public static void main(String[] args) {
+
+        int velikostMrizky = 5;
+
+        for (int radek = 0; radek < velikostMrizky; radek++){
+            for (int sloupec = 0; sloupec < velikostMrizky; sloupec++) {
+                if (radek == velikostMrizky -1 || radek == 0 || radek == sloupec) {
+                    System.out.print(" x ");
+                }
+                else {
+                    System.out.print(" ■ ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
