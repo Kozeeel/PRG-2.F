f900877107e72c61a15e3bb156c63dae2340f231
daniel.kozlik <daniel.kozlik@infis.cz>
2025-01-06 13:16:07 +0100

    dgdgd

---
+++ src/leden/metody.java
@@ -0,0 +1,24 @@
+package leden;
+
+import java.util.Random;
+
+public class metody {
+
+//    static void hodKostkou() {
+//        Random rng = new Random();
+//        int hod3 = rng.nextInt(5, 12);
+//        hod3 += 1;
+//        System.out.println("Hod: " + hod3);
+//    }
+
+    static void pozdrav() {
+        System.out.println("Zadejte číslo od 0-10");
+    }
+    public static void main(String[] args) {
+//        hodKostkou();
+
+        pozdrav();
+        pozdrav();
+        pozdrav();
+    }
+}
