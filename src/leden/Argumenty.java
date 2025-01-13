dc63985e3c17e03396e07293de28b4dc71c0e499
daniel.kozlik <daniel.kozlik@infis.cz>
2025-01-13 13:15:30 +0100

    lll

---
+++ src/leden/Argumenty.java
@@ -0,0 +1,28 @@
+package leden;
+
+public class Argumenty {
+
+    static void doubleA(int a){
+        a = a * 2;
+        System.out.println(a);
+    }
+
+    static void pridatKonec(StringBuilder text) {
+        text.append(" konec");
+    }
+    public static void main(String[] args) {
+//        int a = 5;
+//        doubleA(a);
+//
+//        System.out.println(a);
+
+
+        StringBuilder mujText = new StringBuilder("Začátek");
+
+        System.out.println(mujText);
+
+        pridatKonec(mujText);
+
+        System.out.println(mujText);
+    }
+}
