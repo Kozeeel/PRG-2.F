1c71d6b3c53e8a999f2af3ee5d5ba48060fe37a3
daniel.kozlik <daniel.kozlik@infis.cz>
2025-03-03 13:19:05 +0100

    lll

--- src/Březen/Test_cv.java
+++ src/Březen/Test_cv.java
@@ -27,19 +27,19 @@ public class Test_cv {
         return a * mocnina(a, b - 1);
     }
 
-    static double faktorial(int cislo){
-        int vysledek = 1;
-
-        for (int i = 1; i < cislo; i++) {
-            vysledek = vysledek * 1;
-        }
-        return vysledek;
-    }
+//    static double faktorial(int cislo){
+//        int vysledek = 1;
+//
+//        for (int i = 1; i < cislo; i++) {
+//            vysledek = vysledek * 1;
+//        }
+//        return vysledek;
+//    }
     public static void main(String[] args) {
         System.out.println(soucet());
         System.out.println(isTrue(5));
         System.out.println(zprava("dabda"));
         System.out.println(mocnina(2, 5));
-        System.out.println(faktorial(5));
+//        System.out.println(faktorial(5));
     }
 }
