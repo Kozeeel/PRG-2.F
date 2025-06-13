85925393c56345c4eb3e2fc66c307adf84e86729
daniel.kozlik <daniel.kozlik@infis.cz>
2025-06-13 10:35:28 +0200

    fbhf

--- src/Cerven/Teacher.java
+++ src/Cerven/Teacher.java
@@ -3,6 +3,7 @@ package Cerven;
 public class Teacher extends Person{
     String classroom;
 
+    @Override
     public void introduce(){
         super.introduce();
         System.out.println("Učím třídu " + classroom);
